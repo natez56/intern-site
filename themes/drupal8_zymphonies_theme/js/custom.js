8dd15634d8c42e4b05ee147133db5d6447bf6c31
ghaash <daniel.silverman@me.com>
2019-03-04 09:52:10 -0800

    found selector jquery

--- themes/drupal8_zymphonies_theme/js/custom.js
+++ themes/drupal8_zymphonies_theme/js/custom.js
@@ -18,16 +18,32 @@ jQuery(document).ready(function($){
 	//flexslider
 	jQuery('.flexslider').flexslider({
     	animation: "slide"	
-    });
+		});
+
 		
-	// $('selector').onmousehover( function(){
+/* <ul class="test-class sm-nowrap" id="sm-1551718174147889-2" role="group" aria-hidden="true" aria-labelledby="sm-1551718174147889-1" aria-expanded="false" style="width: auto; display: none; top: auto; left: 0px; margin-left: 0px; margin-top: 0px; min-width: 10em; max-width: 20em;">
+ <li class="c-menu-__item">
+<a href="/home/submenu" title="testing twig out">sub home menu</a>
+</li>
+</ul>*/
+		
+	// $('#sm-1551718174147889-2').onmousehover( function(){
 	// 	// event.preventDefault() ??? 
- 	// $(this).find("div").html(
-	// 	<div>
-	// 		<a href="/home/submenu1">home submenu1</a>
-	// 		<a href="/home/submenu2">home submenu2</a>
-	// 		<a href="/home/submenu3">home submenu3</a>
-	// 	</div>
+	// 	alert('this is the correct selector');
+ 	// // $(this).find("div").html(
+	// // 	<div>
+	// // 		<a href="/home/submenu1">home submenu1</a>
+	// // 		<a href="/home/submenu2">home submenu2</a>
+	// // 		<a href="/home/submenu3">home submenu3</a>
+	// // 	</div>
 	// )
 	// })
+
+	// <a href="/" data-target="#" data-toggle="dropdown" class="has-submenu" id="sm-15517204866664756-1" aria-haspopup="true" aria-controls="sm-15517204866664756-2" aria-expanded="false"><span class="sub-arrow">+</span>
+  //                               Home
+  //                           </a>
+
+	$('.has-submenu').click(function() {
+		alert('this is working now, maybe?')
+	});
 });
